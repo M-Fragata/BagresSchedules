a94225d0b5724fec483d91d334325efe08cfc627
Matheus Fragata de Moraes <matheusdemoraes2@gmail.com>
2025-12-11 20:01:53 -0300

    ajustando os estilos

--- src/scripts/loadHours.js
+++ src/scripts/loadHours.js
@@ -61,6 +61,8 @@ export function renderHours(hours, targetId) {
 
     const targetElement = document.querySelector('#hours')
     const targetMorning = document.querySelector('#morning')
+    const targetAfternoon = document.querySelector('#afternoon')
+    const targetNight = document.querySelector('#night')
 
     if (!targetElement) {
 
@@ -70,36 +72,50 @@ export function renderHours(hours, targetId) {
 
     }
 
-    const hoursHtml = hours.map(hour => {
+    const hoursMorning = hours.map(hour => {
 
-        const [hourNumber,] = hour.split(":")
+        const [hourNumber] = hour.split(":")
 
         if (Number(hourNumber) >= 8 && Number(hourNumber) <= 11) {
             //<li data-period="morning" class="hour-period">Manhã</li>
             return `
-
+            <div class="hour-schedule">
             <li data-period="morning" value="${hour}" class="hour hour-available">${hour}</li>
-
-            `
-        } else if (Number(hourNumber) <= 17) {
-            //<li data-period="afternoon" class="hour-period">Tarde</li>
-            return `
-
-            
-
-            <li data-period="afternoon" value="${hour}" class="hour hour-available">${hour}</li>
-
-            `
-        } else if (Number(hourNumber) <= 21) {
-            //<li data-period="night" class="hour-period">Noite</li>
-            return `
-
-            <li data-period="night" value="${hour}" class="hour hour-available">${hour}</li>
-
+            </div>
             `
         }
 
     }).join("")
 
-    targetElement.innerHTML = hoursHtml
+    const hoursAfternoon = hours.map(hour => {
+
+        const [hourNumber] = hour.split(":")
+
+        if (Number(hourNumber) > 11 && Number(hourNumber) <= 17) {
+            //<li data-period="afternoon" class="hour-period">Tarde</li>
+            return `
+            <div class="hour-schedule">
+            <li data-period="afternoon" value="${hour}" class="hour hour-available">${hour}</li>
+            </div>
+            `
+        }
+    }).join("")
+
+    const hoursNight = hours.map(hour => {
+
+        const [hourNumber] = hour.split(":")
+
+        if (Number(hourNumber) > 17 && Number(hourNumber) <= 21) {
+        //<li data-period="night" class="hour-period">Noite</li>
+        return `
+            <div class="hour-schedule">
+            <li data-period="night" value="${hour}" class="hour hour-available" >${hour}</li>
+            </div>
+            `
+        }
+    }).join("")
+    
+targetMorning.innerHTML = hoursMorning
+targetAfternoon.innerHTML = hoursAfternoon
+targetNight.innerHTML = hoursNight
 }
